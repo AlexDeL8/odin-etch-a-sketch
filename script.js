8927ae64811ba5e6a8ce6cfa02d85b231cee9372
Alex De Luca <alexnaj88@gmail.com>
2022-01-03 16:01:15 -0500

    hover fills cell and no borders for canvas look

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 'use strict'
 
-document.addEventListener("DOMContentLoaded", createSketchGrid());
+document.addEventListener('DOMContentLoaded', createSketchGrid());
 
 function createSketchGrid() {
     let sketchGrid = document.getElementById('sketch-grid');
-    console.log(sketchGrid);
 
     for(let rowCount = 0; rowCount < 16; rowCount++) {
         let rowElement = document.createElement('div');
@@ -14,7 +13,12 @@ function createSketchGrid() {
         for(let columnCount = 0; columnCount < 16; columnCount++) {
             let cellElement = document.createElement('div');
             cellElement.classList.add('cell');
+            cellElement.addEventListener('mouseover', (e) => fillOnMouseOver(e))
             rowElement.appendChild(cellElement);
         }
     }
+}
+
+function fillOnMouseOver(e) {
+    e.target.style.backgroundColor = 'grey'
 }
