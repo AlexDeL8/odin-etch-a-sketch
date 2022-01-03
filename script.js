91080910b96ae497c9f954d05f953cba317a313b
Alex De Luca <alexnaj88@gmail.com>
2022-01-02 22:34:27 -0500

    adding base code for document start and cell size

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict'
+
+document.addEventListener("DOMContentLoaded", createSketchGrid());
+
+function createSketchGrid() {
+    let sketchGrid = document.getElementById('sketch-grid');
+
+    for(let rowCount = 0; rowCount < 16; rowCount++) {
+        let rowElement = document.createElement('div');
+        rowElement.classList.add('row');
+        sketchGrid.appendChild(rowElement);
+
+        for(let columnCount = 0; columnCount < 16; columnCount++) {
+            let cellElement = document.createElement('div');
+            cellElement.classList.add('cell');
+            rowElement.appendChild(cellElement);
+        }
+    }
+}
